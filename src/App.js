4f3474b0624ff52b966fa555c7d48764c827c327
Barry Mamadou <115747234+barrymamadou12345@users.noreply.github.com>
2024-02-07 22:28:50 +0000

    WIP

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
+import React from "react";
+import "./index.css";
 
-import React from 'react';
-import './index.css';
-import './css/Style-App.css';
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <h1 className='text-center font-bold text-[100px] text-red-500 italic'>La liste de mes templates </h1>
+        <h1 className="text-center font-bold mt-40 text-[60px] text-green-500 italic">
+          La liste de mes templates
+        </h1>
       </BrowserRouter>
     </div>
   );
